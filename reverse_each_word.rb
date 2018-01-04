9a75976f6e75fb8c8eb0dc940c457b21936b1e85
PJ Frias <peejfrias@gmail.com>
2018-01-04 01:48:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(string)
   word_array = string.split(" ")
   reverse_words = []
-  binding.pry
   word_array.each {|word| reverse_words << word.reverse}
   reverse_words
+  binding.pry
 end
